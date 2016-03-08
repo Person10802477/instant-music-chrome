6f5e8fa3249bf06f5fd51629fb137e7db784efc0
woniesong92 <hs459@cornell.edu>
2016-03-08 07:37:37 -0800

    Added search results UI

--- foreground/src/components/Header/SearchBar/SearchBar.js
+++ foreground/src/components/Header/SearchBar/SearchBar.js
@@ -10,6 +10,16 @@ class SearchBar extends React.Component {
         <input className="search-input form-control truncate" placeholder="ex. John Lennon Imagine"
           autocomplete="off" spellcheck="false" autocorrect="off" tabindex="1" />
         <i className="fa fa-times clear-icon fa-fw"></i>
+        <div className="search-results">
+          <div className="search-result-item truncate">
+            <div className="result-title">
+              John Lennon
+            </div>
+            <div className="result-artist">
+              Imagine
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
