ae13780bb7b3eba5c31d50297c85e00453f709ee
woniesong92 <hs459@cornell.edu>
2016-03-31 01:16:45 -0700

    Updated gulp and npm

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var del = require('del'); // rm -rf
 var zip = require('gulp-zip');
+var webpack = require('webpack-stream');
 
 gulp.task('default', ['zip'], function() {
   console.log('Your Chrome unpackaged app is inside ./built/');
@@ -11,8 +12,16 @@ gulp.task('clean-built', function() {
   return del(['built']);
 });
 
-gulp.task('clean-archive', function() {
+gulp.task('clean-dist', function() {
   return del(['dist']);
+});
+
+gulp.task('clean-bundle', function() {
+  return del(['./foreground/bundle']);
+});
+
+gulp.task('clean', ['clean-bundle', 'clean-built', 'clean-dist'], function() {
+  console.log("Cleaned foreground/bundle/, built/, dist/ ...");
 });
 
 var filesToMove = [
@@ -30,13 +39,19 @@ var filesToMove = [
   './manifest.json'
 ];
 
-gulp.task('build', ['clean-built', 'clean-archive'], function(){
+gulp.task('webpack-build', ['clean'], function() {
+  return gulp.src('./foreground/src/app.js')
+    .pipe(webpack( require('./webpack.production.config.js') ))
+    .pipe(gulp.dest('./foreground/bundle/'));
+});
+
+gulp.task('move-files', ['webpack-build'], function() {
   return gulp.src(filesToMove, { base: './' })
   .pipe(gulp.dest('built'));
 });
 
-gulp.task('zip', ['build'], function() {
+gulp.task('zip', ['move-files'], function() {
   return gulp.src('built/**/*')
-    .pipe(zip('built.zip'))
+    .pipe(zip('dist.zip'))
     .pipe(gulp.dest('dist'));
 });
