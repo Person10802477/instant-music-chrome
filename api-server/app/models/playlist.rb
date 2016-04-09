e495f5ece4d16f23fb915492b4a36a55727b3e09
woniesong92 <hs459@cornell.edu>
2016-04-09 11:51:33 -0700

    Cleaned repo for gh-pages branch

--- api-server/app/models/playlist.rb
+++
@@ -1,9 +0,0 @@
-class Playlist < ActiveRecord::Base
-  validates :title, presence: true
-
-  belongs_to :user
-
-  has_many :songs, dependent: :destroy
-end
-
-# How do I ensure one user cannot have two playlists with the same title?
--- api-server/app/models/song.rb
+++
@@ -1,5 +0,0 @@
-class Song < ActiveRecord::Base
-  validates :video_id, presence: true
-
-  belongs_to :playlist
-end
