0a214daf56f7ea365c5f056cfa94301f1f16bb52
woniesong92 <hs459@cornell.edu>
2016-04-09 20:14:21 -0700

    Little design changes

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,37 @@
-// $(function(){
-//   $(".main__typing").typed({
-//     strings: ["Beatles Let it be"],
-//     typeSpeed: 50
-//   });
-// });
+function playVideo() {
+  $('video.main__video')[0].play();
+}
+
+function onTypingComplete() {
+  $('.cta').fadeOut(1000, function() {
+    $('.main').fadeIn().removeClass("hidden");
+    playVideo();
+  });
+}
+
+function startTyping() {
+  $(".cta__typing-text").typed({
+    strings: ["^1500 Beatles ^1000 let it be ^500",],
+    typeSpeed: 50,
+    callback: onTypingComplete
+  });
+}
+
+function onInstallClicked(event) {
+  var button = event.target;
+
+  if (chrome.app.isInstalled) {
+    $(button).text("Already installed :D");
+  }
+  
+  chrome.webstore.install("https://chrome.google.com/webstore/detail/bhmdfmjoieebpdomkdfopfpcenomaood", function(success) {
+    $(button).text("Installed :D");
+  }, function(fail) {
+    $(button).text("Failed :(");
+  });
+}
 
 $(function() {
-  $(".main__typing-text").typed({
-    strings: ["", "Beatles Let it be"],
-    typeSpeed: 50
-  });
-})
+  startTyping();
+  $(".main__download-btn").click(onInstallClicked);
+});
