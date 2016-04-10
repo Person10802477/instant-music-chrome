d10685bc2f7a77820d8293208ace4da84f799a37
woniesong92 <hs459@cornell.edu>
2016-04-09 17:43:56 -0700

    Added typing effect

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// $(function(){
+//   $(".main__typing").typed({
+//     strings: ["Beatles Let it be"],
+//     typeSpeed: 50
+//   });
+// });
+
+$(function() {
+  $(".main__typing-text").typed({
+    strings: ["", "Beatles Let it be"],
+    typeSpeed: 50
+  });
+})
