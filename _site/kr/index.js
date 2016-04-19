4f9e968c264de4a4a614314d40debb7492f1471e
woniesong92 <hs459@cornell.edu>
2016-04-18 21:28:22 -0700

    Making changes

---
+++ _site/kr/index.js
@@ -0,0 +1,37 @@
+function playVideo() {
+  $('video.main__video')[0].play();
+}
+
+function onTypingComplete() {
+  $('.cta').fadeOut(1000, function() {
+    $('.main').fadeIn().removeClass("hidden");
+    playVideo();
+  });
+}
+
+function startTyping() {
+  $(".cta__typing-text").typed({
+    strings: ["^1500 Flo Rida ^1000 my house ^500"],
+    typeSpeed: 50,
+    callback: onTypingComplete
+  });
+}
+
+function onInstallClicked(event) {
+  var button = event.target;
+
+  if (chrome.app.isInstalled) {
+    $(button).text("Already installed :D");
+  }
+  
+  chrome.webstore.install("https://chrome.google.com/webstore/detail/bhmdfmjoieebpdomkdfopfpcenomaood", function(success) {
+    $(button).text("Installed :D");
+  }, function(fail) {
+    $(button).text("Failed :(");
+  });
+}
+
+$(function() {
+  startTyping();
+  $(".main__download-btn").click(onInstallClicked);
+});
